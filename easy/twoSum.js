94104b18cf34e2ff557603164ff1ba1d6a1ea40c
efs0-cod3 <eflearnng@gmail.com>
2022-10-21 10:31:30 -0400

    js and ts first challenge

--- easy/twoSum.js
+++ easy/twoSum.js
@@ -3,14 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    let newArr = []	
-       for(let i = 0; i < nums.length; i++){
-           for(let k = 0; k < nums.length; k++){
-                 if(nums[i] + nums[k] === target && i !== k){
-                   newArr.push(i,k)
-                       return newArr
-               } 
-           }
-       }
-   };
+const twoSum = function (nums, target) {
+  let newArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let k = 0; k < nums.length; k++) {
+      if (nums[i] + nums[k] === target && i !== k) {
+        newArr.push(i, k);
+        return newArr;
+      }
+    }
+  }
+};
+twoSum([2,7,11,15],9)
